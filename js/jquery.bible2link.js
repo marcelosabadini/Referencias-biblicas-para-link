35feec89d2883be7e1e9fdaffd780df1d0ddc070
Marcelo Sabadini <marcelosabadini@gmail.com>
2012-10-01 18:54:49 -0300

    Fazendo a primeira versão trabalhar com livros abreviados. Ex: Sl, Jo, 1CO, etc...

--- js/jquery.bible2link.js
+++ js/jquery.bible2link.js
@@ -8,6 +8,16 @@
             bible: 'nvi', // acf(almeida corrigida e revisada fiel), aa (almeida revisada impressa biblica), nvi (nova versao internacional), tb (sociedade bibia britanica)
             target: '',
             more_style: '',
+            valid_books: ['1CO', '1CR', '1JO', '1PE', 
+                '1RS', '1SM', '1TM', '1TS', '2CO', '2CR', 
+                '2JO', '2PE', '2RS', '2SM', '2TM', '2TS', 
+                '3JO', 'AG ', 'AM ', 'AP ', 'AT ', 'CL ', 'CT ', 
+                'DN ', 'DT ', 'EC ', 'ED ', 'EF ', 'ET ', 'EX ', 
+                'EZ ', 'FM ', 'FP ', 'GL ', 'GN ', 'HB ', 'HC ', 
+                'IS ', 'JD ', 'JL ', 'JN ', 'JO ', 'JÓ', 'JR ', 
+                'JS ', 'JZ ', 'LC ', 'LM ', 'LV ', 'MC ', 'ML ', 
+                'MQ ', 'MT ', 'NA ', 'NE ', 'NM ', 'OB ', 'OS ', 
+                'PV ', 'RM ', 'RT ', 'SF ', 'SL ', 'TG ', 'TT ', 'ZC '],
 
             callback: null,
             debug : false
@@ -30,9 +40,14 @@
             if(text != ''){
                 
                 text = text.replace(/((\b[1-3]{0,1}\s{0,1}[a-zA-ZáéíóúâêîôûãẽĩõũÁÉÍÓÚÂÊÎÔÛÃẼŨÕŨ]{2,})\s(([0-9]{1,2})):{0,1}([0-9\-,]*))\b/gim, function($1, $2, $3, $4, $5, $6){
+                    
                     var new_text = '<a href="'+options.url_base+'/'+options.bible+'/'+$3+'/'+$4+'/'+$6+'" style="'+options.more_style+'" target="'+options.target+'">'+$1+'</a>'; // ['+$1+', '+$2+', '+$3+', '+$4+', '+$5+', '+$6+']
-                    // before, needs to check if the biblical name is valid and return the abbreviation
-                    return new_text;
+                    if(options.valid_books.indexOf($3.toUpperCase()) > -1){
+                        return new_text;
+                    } else {
+                        return $1;
+                    }
+                    
                 });
                 
                 // return the new text with the links
