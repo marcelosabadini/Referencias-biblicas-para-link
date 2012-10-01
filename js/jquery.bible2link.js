9a4d1d8a510a939a7c6c8e183c7be392de780e7b
Marcelo Sabadini <marcelosabadini@gmail.com>
2012-10-01 19:28:43 -0300

    Melhoria na lista de livros válidos. Agora já esta funcionando para os livros(PT_BR):
    ['1CO', '1CR', '1JO', '1PE',  '1RS', '1SM', '1TM', '1TS', '2CO', '2CR',  '2JO', '2PE', '2RS', '2SM', '2TM', '2TS',  '3JO', 'AG', 'AM', 'AP', 'AT', 'CL', 'CT',  'DN', 'DT', 'EC', 'ED', 'EF', 'ET', 'EX', 'EZ', 'FM', 'FP', 'GL', 'GN', 'HB', 'HC', 'IS', 'JD', 'JL', 'JN', 'JO', 'JÓ', 'JR', 'JS', 'JZ', 'LC', 'LM', 'LV', 'MC', 'ML', 'MQ', 'MT', 'NA', 'NE', 'NM', 'OB', 'OS', 'PV', 'RM', 'RT', 'SF', 'SL', 'TG', 'TT', 'ZC']

--- js/jquery.bible2link.js
+++ js/jquery.bible2link.js
@@ -11,13 +11,13 @@
             valid_books: ['1CO', '1CR', '1JO', '1PE', 
                 '1RS', '1SM', '1TM', '1TS', '2CO', '2CR', 
                 '2JO', '2PE', '2RS', '2SM', '2TM', '2TS', 
-                '3JO', 'AG ', 'AM ', 'AP ', 'AT ', 'CL ', 'CT ', 
-                'DN ', 'DT ', 'EC ', 'ED ', 'EF ', 'ET ', 'EX ', 
-                'EZ ', 'FM ', 'FP ', 'GL ', 'GN ', 'HB ', 'HC ', 
-                'IS ', 'JD ', 'JL ', 'JN ', 'JO ', 'JÓ', 'JR ', 
-                'JS ', 'JZ ', 'LC ', 'LM ', 'LV ', 'MC ', 'ML ', 
-                'MQ ', 'MT ', 'NA ', 'NE ', 'NM ', 'OB ', 'OS ', 
-                'PV ', 'RM ', 'RT ', 'SF ', 'SL ', 'TG ', 'TT ', 'ZC '],
+                '3JO', 'AG', 'AM', 'AP', 'AT', 'CL', 'CT', 
+                'DN', 'DT', 'EC', 'ED', 'EF', 'ET', 'EX', 
+                'EZ', 'FM', 'FP', 'GL', 'GN', 'HB', 'HC', 
+                'IS', 'JD', 'JL', 'JN', 'JO', 'JÓ', 'JR', 
+                'JS', 'JZ', 'LC', 'LM', 'LV', 'MC', 'ML', 
+                'MQ', 'MT', 'NA', 'NE', 'NM', 'OB', 'OS', 
+                'PV', 'RM', 'RT', 'SF', 'SL', 'TG', 'TT', 'ZC'],
 
             callback: null,
             debug : false
@@ -42,6 +42,7 @@
                 text = text.replace(/((\b[1-3]{0,1}\s{0,1}[a-zA-ZáéíóúâêîôûãẽĩõũÁÉÍÓÚÂÊÎÔÛÃẼŨÕŨ]{2,3})\s{0,1}(([0-9]{1,2})):{0,1}([0-9\-,]{0,}))\b/gim, function($1, $2, $3, $4, $5, $6){
                     
                     var new_text = '<a href="'+options.url_base+'/'+options.bible+'/'+$3+'/'+$4+'/'+$6+'" style="'+options.more_style+'" target="'+options.target+'">'+$1+'</a>'; // ['+$1+', '+$2+', '+$3+', '+$4+', '+$5+', '+$6+']
+                    
                     if(options.valid_books.indexOf($3.toUpperCase()) > -1){
                         return new_text;
                     } else {
